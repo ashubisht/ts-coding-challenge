0d59aebcf66d4bcc06a46855ff8b9327bcc304b6
ashubisht <19997964+ashubisht@users.noreply.github.com>
2025-05-23 01:22:36 +0530

    Implement code to inject balance to accounts pre-mentioned

---
+++ src/inject-balance.ts
@@ -0,0 +1,43 @@
+import {config} from "dotenv";
+import { AccountBalanceQuery, AccountId, Client, Hbar, HbarUnit, PrivateKey, TransferTransaction } from "@hashgraph/sdk"
+import {accounts} from "./config";
+
+config();
+
+export async function validateAndPrefillBalance(){
+  const recipientAccountId = AccountId.fromString(accounts[0].id);
+  const senderAccountId = AccountId.fromString(<string>process.env.MY_ACCOUNT_ID);
+  const senderPrivateKey = PrivateKey.fromStringECDSA(<string>process.env.MY_PRIVATE_KEY);
+
+  const client = Client.forTestnet().setOperator(senderAccountId, senderPrivateKey);
+  const balanceQuery = await new AccountBalanceQuery().setAccountId(recipientAccountId).execute(client);
+  const balance = balanceQuery.hbars.toTinybars();
+  const targetBalance = Hbar.from(10, HbarUnit.Hbar).toTinybars();
+  const topUp = targetBalance.subtract(balance);
+
+  if(topUp.lessThanOrEqual("0")){
+    console.log(`Amount need not be prefilled. Account id ${recipientAccountId} has enough balance: ${balanceQuery.hbars.toString(HbarUnit.Hbar)}`);
+    client.close();
+    return;
+  }
+
+  const transferTransaction = await new TransferTransaction()
+    .addHbarTransfer(senderAccountId, Hbar.fromTinybars(topUp).negated())
+    .addHbarTransfer(recipientAccountId, Hbar.fromTinybars(topUp))
+    .execute(client);
+  
+  const receipt = await transferTransaction.getReceipt(client);
+  client.close(); 
+  console.log("Transfer status:", receipt.status.toString());
+  console.log(`Refilled ${Hbar.fromTinybars(topUp)} HBAR`);
+}
+
+validateAndPrefillBalance().then(()=> {
+  console.log("Executed");
+}).catch((err)=> {
+  if (err instanceof Error){
+    console.error(err.message);
+  }else{
+    JSON.stringify(err);
+  }
+});
