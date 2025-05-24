dd1567f8749d82d13c0189a23aed0ade76bf0a6c
ashubisht <19997964+ashubisht@users.noreply.github.com>
2025-05-25 05:21:46 +0530

    Implement tokens test case and fix create token for flexibility in test case

--- features/step_definitions/token-service.ts
+++ features/step_definitions/token-service.ts
@@ -1,13 +1,13 @@
 import { Given, Then, When } from "@cucumber/cucumber";
 import { accounts } from "../../src/config";
-import { createToken } from "../../src/token";
-import { AccountBalanceQuery, AccountId, Client, PrivateKey, TokenInfoQuery } from "@hashgraph/sdk";
+import { createToken, mintToken } from "../../src/token";
+import { AccountBalanceQuery, AccountId, Client, Long, PrivateKey, TokenInfoQuery } from "@hashgraph/sdk";
 import assert from "node:assert";
 
 const client = Client.forTestnet();
 
 Given(/^A Hedera account with more than (\d+) hbar$/, async function (expectedBalance: number) {
-  const account = accounts[0]
+  const account = accounts[0];
   const MY_ACCOUNT_ID = AccountId.fromString(account.id);
   const MY_PRIVATE_KEY = PrivateKey.fromStringED25519(account.privateKey);
   client.setOperator(MY_ACCOUNT_ID, MY_PRIVATE_KEY);
@@ -15,7 +15,7 @@ Given(/^A Hedera account with more than (\d+) hbar$/, async function (expectedBa
 //Create the query request
   const query = new AccountBalanceQuery().setAccountId(MY_ACCOUNT_ID);
   const balance = await query.execute(client);
-  assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance, `Account balance is: ${balance.hbars.toBigNumber().toNumber()}`)
+  assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance, `Account balance is: ${balance.hbars.toBigNumber().toNumber()}`);
 });
 
 When(/^I create a token named Test Token \(HTT\)$/, async function () {
@@ -27,7 +27,7 @@ When(/^I create a token named Test Token \(HTT\)$/, async function () {
 
   // Not modifying test case statement above, 
   // so using hardcoded values in this function instead of parameters as used in other test cases
-  const tokenTransaction = await createToken("Test Token", "HTT", 2, client);
+  const tokenTransaction = await createToken("Test Token", "HTT", 2, true, client);
   const receipt = await tokenTransaction.getReceipt(client)
   this.tokenId = receipt.tokenId;
   assert.ok(this.tokenId, "Received invalid tokenId");
@@ -45,7 +45,7 @@ Then(/^The token has the symbol "([^"]*)"$/, async function (symbol: string) {
 
 Then(/^The token has (\d+) decimals$/, async function (decimals: number) {
   const tokenInfo = await new TokenInfoQuery().setTokenId(this.tokenId).execute(client);
-  assert.ok(tokenInfo.decimals == decimals)
+  assert.ok(tokenInfo.decimals == decimals);
 });
 
 Then(/^The token is owned by the account$/, async function () {
@@ -56,29 +56,63 @@ Then(/^The token is owned by the account$/, async function () {
   assert.ok(tokenInfo.treasuryAccountId.equals(clientOperator.accountId));
 });
 
-Then(/^An attempt to mint (\d+) additional tokens succeeds$/, async function () {
+Then(/^An attempt to mint (\d+) additional tokens succeeds$/, async function (amount: number) {
+  const privateKey = PrivateKey.fromStringED25519(accounts[0].privateKey);
+  const mintTransaction = await mintToken(amount, this.tokenId, privateKey, client);
+  const receipt = await mintTransaction.getReceipt(client);
+  assert.strictEqual(receipt.status.toString(), "SUCCESS", "Minting failed");
 
+  const tokenInfo = await new TokenInfoQuery().setTokenId(this.tokenId).execute(client);
+  assert.strictEqual(tokenInfo.totalSupply.toBigInt(), BigInt(amount), "Token supply does not match");
 });
-When(/^I create a fixed supply token named Test Token \(HTT\) with (\d+) tokens$/, async function () {
 
+When(/^I create a fixed supply token named Test Token \(HTT\) with (\d+) tokens$/, async function (totalSupply: number) {
+  const tx = await createToken("Test Token", "HTT", 2, false, client, BigInt(totalSupply));
+  const receipt = await tx.getReceipt(client);
+  this.tokenId = receipt.tokenId;
+
+  assert.ok(this.tokenId, "Failed to create token");
 });
-Then(/^The total supply of the token is (\d+)$/, async function () {
-
+Then(/^The total supply of the token is (\d+)$/, async function (totalSupply: number) {
+  const tokenInfo = await new TokenInfoQuery().setTokenId(this.tokenId).execute(client);
+  assert.strictEqual(tokenInfo.totalSupply.toBigInt(), BigInt(totalSupply), "Total supply of token does not match");
 });
 Then(/^An attempt to mint tokens fails$/, async function () {
-
+  const privateKey = PrivateKey.fromStringED25519(accounts[0].privateKey);
+  const mintTransaction = await mintToken(1, this.tokenId, privateKey, client);
+  
+  await assert.rejects(()=>mintTransaction.getReceipt(client), (err: Error) => {return err.message.includes("TOKEN_HAS_NO_SUPPLY_KEY")} , "Expected mint transaction to fail with message TOKEN_HAS_NO_SUPPLY");
 });
-Given(/^A first hedera account with more than (\d+) hbar$/, async function () {
+Given(/^A first hedera account with more than (\d+) hbar$/, async function (expectedBalance: number) {
+  // Repeat test case???? Similar to TC1: Given hedera account
+  const operator = client.getOperator();
+  assert.ok(operator, "Client operator is null or undefined");
 
+  //Create the query request
+  const query = new AccountBalanceQuery().setAccountId(operator.accountId);
+  const balance = await query.execute(client);
+  assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance, `Account balance is: ${balance.hbars.toBigNumber().toNumber()}`);
 });
 Given(/^A second Hedera account$/, async function () {
-
+  const secondAccount = {
+    id: accounts[1].id,
+    privateKey: PrivateKey.fromStringED25519(accounts[1].privateKey)
+  }
+  this.secondClient = Client.forTestnet().setOperator(secondAccount.id, secondAccount.privateKey);
+  assert.equal(this.secondClient.getOperator()?.accountId.toString(), secondAccount.id, "Unable to set second Hedera account");
 });
-Given(/^A token named Test Token \(HTT\) with (\d+) tokens$/, async function () {
-
+Given(/^A token named Test Token \(HTT\) with (\d+) tokens$/, async function (totalSupply: number) {
+  const tokenTransaction = await createToken("Test Token", "HTT", 2, true, client, BigInt(totalSupply));
+  const receipt = await tokenTransaction.getReceipt(client)
+  this.tokenId = receipt.tokenId;
+  assert.ok(this.tokenId, "Received invalid tokenId");
 });
-Given(/^The first account holds (\d+) HTT tokens$/, async function () {
-
+Given(/^The first account holds (\d+) HTT tokens$/, async function (expectedBalance: number) {
+  const balance = await new AccountBalanceQuery().setAccountId(accounts[0].id).execute(client);
+  assert.ok(balance.tokens, "Expected account to hold tokens but none found");
+  const tokenbalance = balance.tokens.get(this.tokenId);
+  assert.ok(tokenbalance, "Token balance not found or token does not exists");
+  assert.equal(tokenbalance.toNumber(), expectedBalance, "Token balance and expected balance does not match");
 });
 Given(/^The second account holds (\d+) HTT tokens$/, async function () {
 
--- src/token.ts
+++ src/token.ts
@@ -1,16 +1,40 @@
-import { Client, TokenCreateTransaction } from "@hashgraph/sdk";
+import { Client, Long, PrivateKey, TokenCreateTransaction, TokenId, TokenMintTransaction } from "@hashgraph/sdk";
 
-export async function createToken(name: string, symbol: string, decimal: number, client: Client) {
+export async function createToken(name: string, symbol: string, decimal: number, isMintable: boolean, client: Client, initialSupply?: bigint) {
   const operator = client.getOperator();
   if(!operator){
     throw new Error("Client operator is null or undefined");
   }
 
-  return new TokenCreateTransaction()
+  const tokenCreateTx = new TokenCreateTransaction()
     .setTokenName(name)
     .setTokenSymbol(symbol)
     .setDecimals(decimal)
     .setAdminKey(operator.publicKey)
-    .setTreasuryAccountId(operator.accountId)
-    .execute(client);
+    .setTreasuryAccountId(operator.accountId);
+
+  if(initialSupply){
+    tokenCreateTx.setInitialSupply(Long.fromBigInt(initialSupply))
+  }
+
+  if(isMintable){
+    tokenCreateTx.setSupplyKey(operator.publicKey);
+  }
+
+  return tokenCreateTx.execute(client);
+}
+
+export async function mintToken(amount: number, tokenId: TokenId, privateKey: PrivateKey, client: Client ){
+   const operator = client.getOperator();
+  if (!operator) {
+    throw new Error("Client operator is null or undefined");
+  }
+
+  const tokenMintTransaction = await new TokenMintTransaction()
+    .setTokenId(tokenId)
+    .setAmount(amount)
+    .freezeWith(client)
+    .sign(privateKey);
+
+  return tokenMintTransaction.execute(client);
 }
